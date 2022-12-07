08c6d4fbb2a13c5cb3f8ebb1f6efd703dd0c581a
Hash-Key-Uch <momahuchechukwu1@gmail.com>
2022-12-07 13:40:41 -0800

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -15,6 +15,7 @@ def roman_to_int(roman_string):
 
     rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     list_keys = list(rom_n.keys())
+
     num = 0
     last_rom = 0
     list_num = [0]
